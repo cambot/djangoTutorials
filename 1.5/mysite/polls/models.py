0ca190b15c048439c1da812ea75c625ccff6bfdb
Cameron <jcmaci03@gmail.com>
2013-06-30 20:59:26 -0500

    identify calcuated field as a boolean

--- 1.5/mysite/polls/models.py
+++ 1.5/mysite/polls/models.py
@@ -9,6 +9,7 @@ class Poll(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 	was_published_recently.admin_order_field = 'pub_date'  # allows calculated field to be sortable in admin screen.
+	was_published_recently.boolean = True   # green check mark
 	def __unicode__(self):
 		return self.question
 
