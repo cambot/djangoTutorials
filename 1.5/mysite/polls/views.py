f7d5fe840d3675d44a629b27dfec6c7a71bfc7f6
Cameron <jcmaci03@gmail.com>
2013-07-05 20:07:31 -0500

    add dumb views for polls model

--- 1.5/mysite/polls/views.py
+++ 1.5/mysite/polls/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Poll Index")
+
+def detail(request, poll_id):
+	return HttpResponse("details of poll %s" % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("results for poll %s" % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("vote on poll %s" % poll_id)
