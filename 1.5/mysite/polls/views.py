afda39ff9bf2bb1af02c6931daaccf73b176e82a
Cameron <jcmaci03@gmail.com>
2013-07-14 11:24:34 -0500

    add Polls index template

--- 1.5/mysite/polls/views.py
+++ 1.5/mysite/polls/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 def index(request):
-	return HttpResponse("Poll Index")
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+		'latest_poll_list': latest_poll_list,
+	})
+	return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
 	return HttpResponse("details of poll %s" % poll_id)
