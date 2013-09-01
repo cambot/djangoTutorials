767f3cff8c638f05a10e8313c9f14ed83cd5fc8e
Cameron <jcmaci03@gmail.com>
2013-09-01 13:20:29 -0500

    convert poll-index view to generic view

--- 1.5/mysite/polls/views.py
+++ 1.5/mysite/polls/views.py
@@ -2,8 +2,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 #from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Poll, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
 
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
