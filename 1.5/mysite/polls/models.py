98d2fde98f19f5a233d3027e422610fae5b276be
Cameron <jcmaci03@gmail.com>
2013-09-01 15:34:29 -0500

    fix was_published_recently bug for future dates, add test case

--- 1.5/mysite/polls/models.py
+++ 1.5/mysite/polls/models.py
@@ -7,7 +7,8 @@ class Poll(models.Model):
 	question = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now > self.pub_date >= now - datetime.timedelta(days=1)
 	was_published_recently.admin_order_field = 'pub_date'  # allows calculated field to be sortable in admin screen.
 	was_published_recently.boolean = True   # green check mark
 	was_published_recently.short_description = "Published recently?"
